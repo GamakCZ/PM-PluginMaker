6d99ab14f39c310cc684be414eefe15ee4644266
VixikHD <stehliik.vojtech@gmail.com>
2019-05-14 17:58:43 +0200

    Initial commit

---
+++ editor.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>PocketMine Plugin maker</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Actor">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="d-flex flex-row flex-grow-1 align-items-center header" style="padding-left: 15px;padding-bottom: 11px;padding-top: 11px;">
+        <h1 class="display-4 text-uppercase text-center align-items-center align-content-center align-self-center m-auto" style="font-family: Actor, sans-serif;font-size: 22px;">PocketMine Plugin Maker</h1>
+        <div class="mx-auto"><button class="btn btn-primary text-center float-right flex-row mx-auto" id="header-button" type="button">New Project</button><button class="btn btn-primary text-center float-right flex-row" id="header-button" type="button" style="margin-right: 14px;">Save Project</button></div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>PocketMine Plugin maker</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Actor">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="d-flex flex-row flex-grow-1 align-items-center header" style="padding-left: 15px;padding-bottom: 11px;padding-top: 11px;">
+        <h1 class="display-4 text-uppercase text-center align-items-center align-content-center align-self-center m-auto" style="font-family: Actor, sans-serif;font-size: 22px;">PocketMine Plugin Maker</h1><button class="btn btn-primary text-center float-right flex-row mx-auto" id="header-button" type="button">New Project</button></div>
+    <div>
+        <form class="description-form" style="margin-right: 20%;margin-top: 53px;margin-left: 20%;padding-bottom: 0px;">
+            <h4 style="font-family: Actor, sans-serif;font-size: 21px;padding-bottom: 6px;">Plugin description</h4><input class="form-control" type="text" placeholder="PluginName" style="margin-bottom: 5px;"><input class="form-control" type="text" placeholder="Version" style="margin-bottom: 5px;"><input class="form-control" type="text"
+                placeholder="Author" value="VixikHD" readonly="" style="margin-bottom: 5px;"><input class="form-control" type="text" placeholder="Description" style="margin-bottom: 5px;"><input class="form-control" type="text" placeholder="Api" value="3.0.0"
+                style="margin-bottom: 5px;"><button class="btn btn-primary" type="button">Go to the editor</button></form>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
